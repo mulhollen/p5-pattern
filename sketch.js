8e07e95a489f00c3d2cb41670177e17056b949c4
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-02-07 09:47:54 -0600

    clean up

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
+
+// 10 print design
+
+let x = 0;
+let y = 0;
+spacing = 30;
+
 function setup() {
   // put setup code here
+  createCanvas(640, 480);
+  background(0);
 }
 
 function draw() {
   // put drawing code here
-  ellipse(50, 50, 80, 80)
-  console.log("hey gurl hey")
+
+  if (random(1) < 0.5) {
+    stroke(255, 204, 0);
+    line(x, y, x + spacing, y + spacing);
+  } else {
+    stroke(255);
+    line(x, y + spacing, x + spacing, y);
+  }
+  x = x + spacing;
+  if (x > width) {
+    x = 0;
+    y = y + spacing;
+  }
 }
